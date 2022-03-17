aa9b129f3fc4de5fd742869fdba0b9f4a5f280b3
latincoder18 <avaldesa007@gmail.com>
2022-03-17 09:58:22 -0400

    Updates

--- routes/observations.js
+++ routes/observations.js
@@ -7,12 +7,12 @@ const { validateJWT } = require('../middlewares/validateJWT')
 const { isRole } = require('../middlewares/validateRol');
 const router = Router();
 
-router.get('/:id', [validateJWT, check('id', 'El id de mongo debe ser valido').isMongoId(), validateFields], getObservation);
+router.get('/:id', [validateJWT,isRole('ADMIN_ROLE', 'TRAINEE_ROLE'),check('id', 'El id de mongo debe ser valido').isMongoId(), validateFields], getObservation);
 router.get('/user/:id', [validateJWT, check('id', 'El id de mongo debe ser valido').isMongoId(), validateFields], getObservations);
 
 router.post('/', [
     validateJWT,
-    isRole('ADMIN_ROLE'),
+    isRole('ADMIN_ROLE', 'TRAINEE_ROLE'),
     check('client', 'Debes enviar el id del cliente').not().isEmpty().bail().custom(existClient),
     check('observations', 'Debes enviar almenos una observacion').not().isEmpty(),
     check('client','Debe introducir un Id de mongo correcto').isMongoId(),
@@ -22,7 +22,7 @@ router.post('/', [
 router.delete('/:id', [
     validateJWT,
     check('id', 'El id de mongo debe ser valido').isMongoId(),
-    isRole('ADMIN_ROLE'),
+    isRole('ADMIN_ROLE', 'TRAINEE_ROLE'),
     validateFields],
     removeObservation);
 
